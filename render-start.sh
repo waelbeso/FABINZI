ebd48bee14c330e9e32d3ccfef18f163afd94232
Wael El-Begearmi <waelabbas@live.com>
2026-08-29 09:19:46 +0300

    fix: add Render web startup script

---
+++ render-start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -eu
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+exec gunicorn config.wsgi:application \
+  --bind "0.0.0.0:${PORT:-10000}" \
+  --workers "${GUNICORN_WORKERS:-2}" \
+  --timeout "${GUNICORN_TIMEOUT:-120}"
